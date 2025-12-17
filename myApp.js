66b29b4f8ee8a66ca159dc0ba2fbc44847bbe4be
ChaimaSahli <st.chaima25@gmail.com>
2025-12-17 16:08:40 +0000

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -4,7 +4,9 @@ let app = express();
 // app.get("/", function (req, res) {
 //   res.send("Hello Express");
 // });
-console.log("html file");
+console.log("Serve Static Assets");
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", function (req, res) {
   const absolutePath = __dirname + "/views/index.html";
 
